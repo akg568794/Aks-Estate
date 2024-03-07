6f19846f2968cd9b0c77e8fe4ac247935e5e7b68
abhay gupta <akg568794@gmail.com>
2024-03-07 18:24:43 +0530

    A fullstack real state application using MERN and tailwind css

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type: String,
+        required:true,
+        unique:true,
+
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+
+    },
+    password:{
+        type: String,
+        required:true,
+    },
+    avatar:{
+        type: String,
+        default: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+    },
+
+},{timestamps:true});
+
+const User=mongoose.model('User',userSchema);
+
+export default User;
---
+++ api/routes/listing.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { createListing, getListing,getListings, deleteListing, updateListing} from '../controllers/listing.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id',verifyToken,updateListing);
+router.get('/get/:id',getListing);
+router.get('/get',getListings);
+
+
+export default router;
+    
