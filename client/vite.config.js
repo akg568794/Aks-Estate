5c1d668e6efd364f8827da9768c44fba60f59f51
abhay gupta <akg568794@gmail.com>
2024-03-07 22:18:00 +0530

    remove build rollup option

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['swiper/react']
-    }
-  },
   server:{
     proxy:{
       '/api':{
