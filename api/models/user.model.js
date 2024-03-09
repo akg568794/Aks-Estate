52de736482b810a7c9c94c6516069ce45485093c
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-03-09 16:03:40 +0530

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar:{
       type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+      default: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
     },
   },
   { timestamps: true }
