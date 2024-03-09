f214882278e0b2984e1456bc25db93f5c91da5d7
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-03-09 16:47:07 +0530

    Update Listing.jsx

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -91,9 +91,7 @@ export default function Listing() {
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
-              {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+              {listing.regularPrice}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
