7810b50a22b53ab62340dfe6e4cd94deb1d9e30c
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-03-09 14:42:59 +0530

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,29 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type: String,
-        required:true,
-        unique:true,
-
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        required:true,
-        unique:true,
-
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
     avatar:{
-        type: String,
-        default: "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png"
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
-
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
