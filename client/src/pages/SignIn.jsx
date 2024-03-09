b67211224d9247e4f728ea4028305136fd20b2de
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-03-09 15:32:50 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
-import OAuth from '../components/Oauth';
+import Oauth from '../components/Oauth';
 
 const SignIn = () => {
   const[formData,setFormData]=useState({});
