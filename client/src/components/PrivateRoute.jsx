b41f1ee0c7438bbe7bb40b91bb6c8dd13208b613
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-03-09 14:45:39 +0530

    Update PrivateRoute.jsx

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Outlet , Navigate} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
-    const {currentUser}=useSelector((state)=>state.user);
-  return (
-    currentUser ? <Outlet/>: <Navigate to='/sign-in'/>
-  )
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }
